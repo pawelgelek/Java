12f80f460dccb365f770f9a28c82196cdb336806
Paweł <pawelgelek@gmail.com>
2022-12-08 19:09:38 +0100

    zad 4 -> przerobić aby komunikat był zwracany z metody (zamiast void).

--- src/main/java/Brudnopis.java
+++ src/main/java/Brudnopis.java
@@ -1,5 +1,49 @@
 import java.util.Scanner;
 
-public class Brudnopis {}
+public class Brudnopis {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double a = getDouble(scanner, "Podaj pierwsza liczbe");
+        double b = getDouble(scanner, "Podaj druga liczbe");
+        String podaneZKonsoli = getString(scanner);
+        obliczenia(a, b, podaneZKonsoli);
+        wyswietlWynik();
+    }
+
+    private static void obliczenia(double a, double b, String podaneZKonsoli) {
+        switch (podaneZKonsoli) {
+            case "srednia": {
+                double s = a + b / 2;
+                break;
+            }
+            case "pierwiastek": {
+                double p1 = Math.sqrt(a);
+                double p2 = Math.sqrt(b);
+                break;
+            }
+            case "potegowanie": {
+                double p3 = Math.pow(a, b);
+                break;
+            }
+        }
+    }
+
+    private static String getString(Scanner scanner) {
+        System.out.println("Podaj rodzaj wykonywanej operacji:");
+        String podaneZKonsoli = scanner.next();
+        return podaneZKonsoli;
+    }
+
+    private static double getDouble(Scanner scanner, String komunikat) {
+        System.out.println(komunikat);
+        return scanner.nextDouble();
+    }
+
+    private static String wyswietlWynik() {
+        System.out.println();
+        return;
+    }
+
+}
 
     
