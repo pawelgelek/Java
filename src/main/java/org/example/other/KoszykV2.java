732ee50d8b6ae3de2a2adc697ba12136eb10195a
Paweł <pawelgelek@gmail.com>
2023-01-16 20:23:38 +0100

    Zdom -> pobieraj przedmiot / ilosc / cene z konsoli.

--- src/main/java/org/example/other/KoszykV2.java
+++ src/main/java/org/example/other/KoszykV2.java
@@ -17,20 +17,22 @@ public class KoszykV2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int ile = podajLiczbe("Podaj ilosc produktow w koszyku: ", scanner);
-        ProduktV2 produktV2 = coKupujesz();
         ArrayList<ProduktV2> wszystkie = new ArrayList();
-        wszystkie.add(produktV2);
         double suma = 0;
         for (int i = 0; i < ile; i++) {
-            ProduktV2 produktV2 = wszystkie.get(i);
-            wypisz(produktV2,i);
+            String name = podajNazwe("Podaj nazwe: ", scanner);
+            Double cena = podajLiczbe2("Podaj cene produktu: ", scanner);
+            Double ilosc = podajLiczbe2("Podaj liczbe sztuk: ", scanner);
+            ProduktV2 produktV2 = coKupujesz(name, cena, ilosc);
+            wszystkie.add(produktV2);
+//            ProduktV2 produktV2 = wszystkie.get(i);
+//            wypisz(produktV2,i);
 
 
-//            Double cena = podajLiczbe("Podaj cene produktu: ", scanner);
-//            Double ilosc = podajLiczbe("Podaj liczbe sztuk: ", scanner);
-//            suma = suma + (ilosc * cena);
+            suma = suma + (ilosc * cena);
         }
-//        sumaWydatkow(suma);
+        sumaWydatkow(suma);
+        System.out.println(wszystkie);
     }
 
     public static int podajLiczbe(String komunikat, Scanner scanner) {
@@ -39,16 +41,28 @@ public class KoszykV2 {
         return a;
     }
 
+    public static double podajLiczbe2(String komunikat, Scanner scanner) {
+        System.out.println(komunikat);
+        double a = scanner.nextDouble();
+        return a;
+    }
+
     public static ProduktV2 coKupujesz(String name, double price, double quantity) {
-        ProduktV2 produktV2 = new ProduktV2();
-        produktV2.name = name;
-        produktV2.price = price;
-        produktV2.quantity = quantity;
+        ProduktV2 produktV2 = new ProduktV2(name, price, quantity);
+//        produktV2.name = name;
+//        produktV2.price = price;
+//        produktV2.quantity = quantity;
         return produktV2;
     }
 
-//    public static Double sumaWydatkow(double suma) {
-//        System.out.println("suma zakupow: " + suma);
-//        return suma;
-//    }
+    public static String podajNazwe(String komunikat, Scanner scanner) {
+        System.out.println(komunikat);
+        String a = scanner.nextLine();
+        return a;
+    }
+
+    public static Double sumaWydatkow(double suma) {
+        System.out.println("suma zakupow: " + suma);
+        return suma;
+    }
 }
