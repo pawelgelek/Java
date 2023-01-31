0896699c1ed93a321000a6de6094d9c7d9ee4458
Paweł <pawelgelek@gmail.com>
2023-01-31 19:20:57 +0100

    :)

--- src/main/java/org/example/konstruktory/GarageV3.java
+++ src/main/java/org/example/konstruktory/GarageV3.java
@@ -2,19 +2,20 @@ package org.example.konstruktory;
 
 import java.util.HashSet;
 import java.util.Scanner;
-
+//zadanie 2:
+//        - utworz set z lista samochodow uzytkownika Car(name, color)
+//        -- pobierze name i color z konsoli
+//        -- korzystaj z konsturktorow, gettery i settery
 public class GarageV3 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int numberOfCars = getNumber("Podaj liczbe samochodow w garazu: ", scanner);
-        HashSet<String> cars = new HashSet<>();
+        HashSet<CarV3> cars = new HashSet<>();
         for (int i = 0; i < numberOfCars; i++) {
             String name = getName("Podaj marke samochodu: ", scanner);
-            cars.add(name);
-        }
-        for (int j = 0; j < numberOfCars; j++) {
             String color = getName("Podaj kolor samochodu: ", scanner);
-            cars.add(color);
+            CarV3 carV3 = createCar(name, color);
+            cars.add(carV3);
         }
                 System.out.println(cars);
     }
@@ -33,7 +34,7 @@ public class GarageV3 {
     }
 
 
-    public static CarV3 carList(String name, String color) {
+    public static CarV3 createCar(String name, String color) {
         CarV3 carV3 = new CarV3(name, color);
         return carV3;
     }
