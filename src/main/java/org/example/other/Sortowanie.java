95ba4a0931543fd0b4d2804d93aa643f358871d7
Paweł <pawelgelek@gmail.com>
2023-04-24 20:12:56 +0200

    :)

--- src/main/java/org/example/other/Sortowanie.java
+++ src/main/java/org/example/other/Sortowanie.java
@@ -5,18 +5,18 @@ import java.util.Collections;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Sortowanie {
-        public static void main(String[] args) {
-            ArrayList<Integer> numbers = new ArrayList<>();
-            ThreadLocalRandom random = ThreadLocalRandom.current();
-            for (int i = 0; i < 1000; i++) {
-                int number = random.nextInt(10000);
-                numbers.add(number);
-            }
-            System.out.println("Nieposortowane: " + numbers);
-            Collections.sort(numbers);
-            System.out.println("Posortowane rosnaco: " + numbers);
-            Collections.reverse(numbers);
-            System.out.println("Posortowane malejaco: " + numbers);
+    public static void main(String[] args) {
+        ArrayList<Integer> numbers = new ArrayList<>();
+        ThreadLocalRandom random = ThreadLocalRandom.current();
+        for (int i = 0; i < 1000; i++) {
+            int number = random.nextInt(10000);
+            numbers.add(number);
+        }
+        System.out.println("Nieposortowane: " + numbers);
+        Collections.sort(numbers);
+        System.out.println("Posortowane rosnaco: " + numbers);
+        Collections.reverse(numbers);
+        System.out.println("Posortowane malejaco: " + numbers);
 //        int number1 = random.nextInt(10000);
 //        int number2 = random.nextInt(10000);
 //        int number3 = random.nextInt(10000);
@@ -29,9 +29,9 @@ public class Sortowanie {
 //        System.out.println(number4);
 //        System.out.println(number5);
 //        System.out.println(number6);
-        }
-
     }
+
+}
 //     ArrayList<Integer> prices = new ArrayList<>();
 //        prices.add(1); //,3,5,6,7,9,11,32,35,44
 //        prices.add(3);
