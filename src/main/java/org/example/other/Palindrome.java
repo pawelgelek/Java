15f81a00c55aa461a38bb3c8da2681b52e9f9743
Paweł <pawelgelek@gmail.com>
2023-08-19 17:46:27 +0200

    Palindrome housework.

---
+++ src/main/java/org/example/other/Palindrome.java
@@ -0,0 +1,37 @@
+package org.example.other;
+//String Manipulation and Parsing: Tasks involving string manipulation, parsing, and regular expressions are common.
+// Candidates might be asked to reverse a string, check for palindrome, tokenize a string, or perform other string-related operations.
+
+import java.util.Scanner;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String word1 = getString("Podaj wyraz, ktory bedziemy sprawdzac: ", scanner);
+        if(isPalindrome(word1)){
+            System.out.println("Tak, wyraz "+word1+" jest palindromem.");
+        } else {
+            System.out.println("Nie, wyraz "+word1+" nie jest palindromem");
+        }
+    }
+
+    public static String getString(String message, Scanner scanner) {
+        System.out.println(message);
+        String a = scanner.nextLine();
+        return a;
+    }
+
+    public static boolean isPalindrome(String word1) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < word1.length(); i++) {
+            char c = word1.charAt(i);
+            if (Character.isDigit(c) || Character.isAlphabetic(c)) {
+                stringBuilder.append(c);
+            }
+        }
+        return word1.equals(stringBuilder.reverse().toString());
+
+    }
+
+}
+
