547d371a7b90ffd5a71752c709a959ec03987dcc
Paweł <pawelgelek@gmail.com>
2023-10-04 20:56:48 +0200

    Palindrome housework.

--- src/test/java/org/example/other/PalindromeTest.java
+++ src/test/java/org/example/other/PalindromeTest.java
@@ -3,7 +3,10 @@ package org.example.other;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 public class PalindromeTest {
     @Test
@@ -18,10 +21,26 @@ public class PalindromeTest {
         Assertions.assertEquals(false, wynik);
     }
 
+    @Test
+    void isPalindromeNumbers() {
+        boolean wynik = Palindrome.isPalindrome("1");
+        Assertions.assertEquals(true, wynik);
+    }
+
     @ParameterizedTest
-    @MethodSource()
-    void shouldCheckPalindrome(boolean expectedResult, String input) {
+    @MethodSource("provideTestsForPalindrome")
+    void shouldCheckPalindrome(String input, boolean expectedResult) {
         boolean wynik = Palindrome.isPalindrome(input);
         Assertions.assertEquals(expectedResult, wynik);
     }
+
+    private static Stream<Arguments> provideTestsForPalindrome() {
+        return Stream.of(
+                Arguments.of("1", true),
+                Arguments.of("12", false),
+                Arguments.of("5", true)
+        );
+    }
+
+
 }
